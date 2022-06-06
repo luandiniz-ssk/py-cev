b615c85f2869a8d2fad4588792340df19ebff7d2
pedrootavio-gk <gk.pedrootavio@gmail.com>
2022-06-06 09:29:33 -0300

    Atividade #003

---
+++ ex003.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia dois númeors e mostre a soma entre eles
+
+primeiro_valor = int(input('Digite o primeiro valor: '))
+segundo_valor = int(input('Digite o segundo valor: '))
+soma = primeiro_valor + segundo_valor
+print(f'A soma entre {primeiro_valor} e {segundo_valor} é igual a {soma}')
