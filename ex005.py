3c00b869cc233c29e889991411d67a07b461bda9
pedrootavio-gk <gk.pedrootavio@gmail.com>
2022-06-06 12:53:43 -0300

    Atividade #005

---
+++ ex005.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e o seu antecessor
+
+num = int(input('Digite um valor: '))
+antecessor = num - 1
+sucessor = num + 1
+print(f'O número digitado foi {num}, seu antecessor é {antecessor} e o seu sucessor vale {sucessor}')
