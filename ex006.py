8d2c0ff83e2878756a984485acea3ed6d4edb729
ペドロ <106916908+pedrootavio-gk@users.noreply.github.com>
2022-06-06 13:01:20 -0300

    Delete ex006.py

--- ex006.py
+++
@@ -1,9 +0,0 @@
-#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
-
-num = int(input('Digite um valor: '))
-dobro = num * 2
-triplo = num * 3
-raiz_quadrada = num ** 0.5
-print(f'Dobro: {dobro}')
-print(f'Triplo: {triplo}')
-print(f'Raiz quadrada: {raiz_quadrada:.2f}')
