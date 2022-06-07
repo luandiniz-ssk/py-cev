f47d65fd3d8445a895345f49efc923f33f8ee2a4
pedrootavio-gk <gk.pedrootavio@gmail.com>
2022-06-07 10:53:48 -0300

    Atividade #012

---
+++ ex012.py
@@ -0,0 +1,7 @@
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+preço = float(input('Digite o preço do produto: R$'))
+desc = preço * 5 / 100
+novo_preço = preço -(preço * 5 / 100)
+print(f'O produto que custava R${preço:.2f}, com 5% de desconto, passa a custar R${novo_preço:.2f}')
+print(novo_preço)
