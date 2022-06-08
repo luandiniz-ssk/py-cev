b337b1e631396433f26d47ff26d06ca3c080196d
pedrootavio-gk <gk.pedrootavio@gmail.com>
2022-06-08 20:11:07 -0300

    Atividade #026

---
+++ ex026.py
@@ -0,0 +1,12 @@
+#Faça um programa que leia uma frase pelo teclado e mostre:
+#Quantas vezes aparece a letra “A”
+#Em que posição ela aparece a primeira vez
+#Em que posição ela aparece a última vez
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+quantidade = frase.count('A')
+primeira_vez = frase.find('A')
+última_vez = frase.rfind('A')
+print(f'Aparecem {quantidade} letras "A" na frase')
+print(f'Aparece pela primeira vez na {primeira_vez}° posição')
+print(f'Aparece pela última vez na {última_vez}° posição')
